f70cbcb0505bf249d9b4b8cd9bb4e457f292cfa9
TappandoPlay <sfruttailmomento94@tutamail.com>
2025-06-15 02:31:24 +0200

    Create monopoly_go_tool.py

---
+++ monopoly_go_tool.py
@@ -0,0 +1,6 @@
+# Simulazione tool Monopoly Go
+def simulate_dice_generation():
+    print("Monopoly Go Helper Avviato - Dadi in arrivo!")
+
+if __name__ == "__main__":
+    simulate_dice_generation()
